8ebac30e9b084ffd384ed958a537945d289142e8
Snehal Gupta <snehalgupta030@gmail.com>
2025-05-18 19:48:59 +0530

    added code for handling negatives and added testcaes

--- controllers/calculator_controller.rb
+++ controllers/calculator_controller.rb
@@ -1,19 +1,37 @@
 class CalculatorController < ApplicationController
   def index
-    @result = nil
-    if params[:numbers]
-      @result = add(params[:numbers])
+    if params[:numbers].present?
+      @input = params[:numbers]
+
+      begin
+        @result = calculate_sum(@input)
+      rescue => e
+        @error = e.message
+      end
     end
   end
 
   private
 
-  def add(numbers)
-    return 0 if numbers.empty?
+  def calculate_sum(input)
+    delimiter = /,|\n/  # default
 
-    numbers = numbers.gsub("\n", ",") # Convert newlines to commas for easier parsing
-    numbers.split(',').map(&:to_i).sum
+    if input.start_with?("//")
+      header, rest = input.split("\n", 2)
+      custom_delim = header[2]  # e.g., from "//;\n"
+      delimiter = Regexp.escape(custom_delim)
+      input = rest || ""
+    end
+
+    # Convert to array of integers
+    numbers = input.split(/#{delimiter}/).map(&:strip).map(&:to_i)
+
+    # Check for negatives
+    negatives = numbers.select { |n| n < 0 }
+    if negatives.any?
+      raise "negative numbers not allowed: #{negatives.join(', ')}"
+    end
+
+    numbers.sum
   end
 end
-
-# app/views/calculator/index.html.erb
--- services/string_calculator.rb
+++ services/string_calculator.rb
@@ -1,7 +1,17 @@
 class StringCalculator
-  def self.add(numbers)
-    return 0 if numbers.empty?
+  def self.add(input)
+    delimiter = /,|\n/
+    if input.start_with?("//")
+      header, input = input.split("\n", 2)
+      custom_delim = header[2]
+      delimiter = Regexp.escape(custom_delim)
+    end
 
-    numbers.split(/,|\n/).map(&:to_i).sum
+    numbers = input.split(/#{delimiter}/).map(&:strip).map(&:to_i)
+    negatives = numbers.select { |n| n < 0 }
+
+    raise "negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
+
+    numbers.sum
   end
 end
